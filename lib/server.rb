2b0a52c94d2e975abaf139b83312a1f216af9fc9
Sabrina Robinson <sabrinarob@gmail.com>
2018-04-04 21:10:19 -0600

    Updated shutdown method to reduce redundancy.

--- lib/server.rb
+++ lib/server.rb
@@ -5,6 +5,7 @@ require './lib/controller'
 class Server
   attr_reader :tcp_server,
               :request_lines,
+              :shutdown,
               :controller,
               :response
 
@@ -19,65 +20,20 @@ class Server
   def run_server
     until @shutdown
       connection = @tcp_server.accept
-
       self.set_request_lines(connection)
-
       puts "Got a request:"
       puts @request_lines.inspect
-
       headers = @controller.parse_headers(@request_lines)
       response = @controller.response_path(headers)
-
       self.send_response(response, connection)
-
       connection.close
-
       puts "\nResponse sent, now exiting"
       @request_lines = []
       if headers[1] == "/shutdown"
-        shutdown
+        @shutdown = true
       end
     end
   end
-
-  # def parse_headers(request_lines)
-  #   first_request_line = request_lines[0].split(' ')
-  #   verb = first_request_line[0]
-  #   path, params = first_request_line[1].split('?')
-  #   protocol = first_request_line[2]
-  #
-  #   [verb, path, params, protocol] +
-  #   request_lines[1].split(" ")[1].split(":") +
-  #   [ request_lines[6].split(" ")[1] ]
-  # end
-  #
-  # def parse_params(params)
-  #   params_list = params.split("&")
-  #   params_list.each_with_object({}) do |param_pair, memo|
-  #     #memo is {}
-  #     # an example of param_pair: 'word=elephant'
-  #     #could use reduce but will need to explicitely return memo to build a hash instead of a string/
-  #     key, value = param_pair.split('=')
-  #     memo[key] = value
-  #   end
-  # end
-
-  # def response_path(parse_headers)
-  #   verb, path, params, protocol, host, port, accept = parse_headers
-  #   if path == "/"
-  #     self.root_response(parse_headers)
-  #   elsif path == "/hello"
-  #     print "hello"
-  #     self.hello_response
-  #   elsif path == "/datetime"
-  #     response.datetime_response
-  #   elsif path == "/shutdown"
-  #     self.shutdown_response
-  #   elsif path == "/word_search"
-  #     parsed_params = parse_params(params)
-  #     self.word_search_response(parsed_params['word'])
-  #   end
-  # end
 
   def set_request_lines(connection)
     puts "ready for a request"
@@ -85,10 +41,6 @@ class Server
     while (line = connection.gets) and !line.chomp.empty?
       @request_lines << line.chomp
     end
-  end
-
-  def shutdown
-    @shutdown = true
   end
 
   def set_output(response)
@@ -116,40 +68,6 @@ class Server
     connection.puts output
     puts log(output, headers)
   end
-
-  def root_response(parsed_headers)
-    verb, path, protocol, host, port, accept = parsed_headers
-    "<pre>
-    Verb: #{verb}
-    Path: #{path}
-    Protocol: #{protocol}
-    Host: #{host}
-    Port: #{port}
-    Origin: #{host}
-    Accept: #{accept}
-    </pre>"
-  end
-
-  # def hello_response
-  #   "<h1>Hello, World! #{@count += 1}</h1>"
-  # end
-
-  # def datetime_response
-  #   Time.now.strftime('%I:%M on %A, %B %d, %Y')
-  # end
-  #
-  # def shutdown_response
-  #   "Total Requests: #{@count}"
-  # end
-  #
-  # def word_search_response(word)
-  #   dictionary = File.read("/usr/share/dict/words")
-  #   if dictionary.include?(word)
-  #     "#{word} is a known word"
-  #   else
-  #     "#{word} is not a known word"
-  #   end
-  # end
 end
 
 server = Server.new
